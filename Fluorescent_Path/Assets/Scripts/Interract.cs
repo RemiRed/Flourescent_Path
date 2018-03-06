4aa9d4f90ae878c2457cd895758c9cb82816b8d4
DanielBengtssonST <b_leinad@hotmail.com>
2018-03-06 11:30:11 +0100

    Script comments
    Added some comments to script for clarifications

--- Fluorescent_Path/Assets/Scripts/Interract.cs
+++ Fluorescent_Path/Assets/Scripts/Interract.cs
@@ -4,23 +4,24 @@ using UnityEngine;
 
 public class Interract : MonoBehaviour
 { 
-	GameObject carriedObject;							//The 
+	GameObject carriedObject;							//The currently carried object 
 
 	[SerializeField][Range(0.001f, int.MaxValue)]
-    float carryDistance;
+    float carryDistance;								//The distance at which the player interacts with and carries objects 
 	[SerializeField][Range(0.001f, int.MaxValue)]
-    float normalizeSpeed;
+    float normalizeSpeed;								//The speed carried objects return to a normalized position after being dislocated 
 	[SerializeField][Range(0.001f, int.MaxValue)]
 	float slow;
 
-	Vector3 oldPos;
+	Vector3 oldPos;										//The carried objects previous position. Used to calculate position normalization and throwing 
 
-	float defaultDrag;
+	float defaultDrag;									//The players default drag. Used to reset a players movement speed after movement speed adjustments
 
-	bool carrying = false;
+	bool carrying = false;								//If the player is currently carrying an object
 
 	private void Start()
     {
+		//Sets the defaultDrag variable
         defaultDrag = transform.parent.GetComponent<Rigidbody>().drag;
     }
 
@@ -54,8 +55,12 @@ public class Interract : MonoBehaviour
         Ray ray = GetComponent<Camera>().ScreenPointToRay(new Vector3(x, y));
 		RaycastHit hit;
 
+		//Determines if raycast hit an object
 		if (Physics.Raycast (ray, out hit, carryDistance)) {
+			//Determines what kind of relevant object the raycast hit
 			if (hit.transform.tag == "Movable") {
+
+				//sets changes to carry a movable object
 				carrying = true;
 				carriedObject = hit.transform.gameObject;
 				carriedObject.transform.parent = transform;
@@ -73,10 +78,10 @@ public class Interract : MonoBehaviour
 
         }
     }
-
-	//
+		
     void Drop()
     {
+		//Resets changes to to no longer carry an object
 		carriedObject.GetComponent<Rigidbody>().useGravity = true;
 		carriedObject.GetComponent<Rigidbody>().freezeRotation = false; 
 		transform.parent.GetComponent<Rigidbody>().drag = defaultDrag;
