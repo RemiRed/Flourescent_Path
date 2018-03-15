4281b5de144704ec3d72360b0be1124a4d1add25
OskarMagnusson <oskar.magnusson97@gmail.com>
2018-03-15 12:15:02 +0100

    bla
    inte viktiga saker tro mig

---
+++ Fluorescent_Path/Assets/Scripts/Puzzle Scripts/MirrorRoom.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MirrorRoom : RoomVariables {
+
+    [SerializeField]
+    List<GameObject> tiles = new List<GameObject>();
+
+	// Use this for initialization
+	void Start () {     
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void FindPLayers()
+    {
+        foreach (GameObject playa in tiles)
+        {
+            if(playa.GetComponent<PlayerDetectingTiles>().player != null)
+            {
+
+            }
+
+        }
+    }
+}
